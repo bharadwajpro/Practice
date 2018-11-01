6eec22466697b7aaf7d829174092f13e60ef17fe
Krishna Bharadwaj <bharadwaj6598@gmail.com>
2018-11-01 10:27:36 +0530

    Update tree_traversal.cpp

--- tree_traversal.cpp
+++ tree_traversal.cpp
@@ -1,87 +1,79 @@
+/* Binary Tree Traversal - Preorder, Inorder, Postorder */
 #include<iostream>
 using namespace std;
-struct node{
-    int value;
-    struct node* left;
-    struct node* right;
+ 
+struct Node {
+	char data;
+	struct Node *left;
+	struct Node *right;
 };
-node* newNode(int val){
-    node* temp=(node*)malloc(sizeof(struct node));
-    temp->value=val;
-    temp->left=NULL;
-    temp->right=NULL;
-    return temp;
-}
-node* createTree(){
-    int i,j,n,a;
-    cin>>n;
-    cin>>a;
-    i=0;
-    node* q[n];j=0;
 
-    q[j]=newNode(a);j=j+1;i=i+1;
-    //cout<<q[j-1]->value<<endl;
+//Function to visit nodes in Preorder
+void Preorder(struct Node *root) {
+	// base condition for recursion
+	// if tree/sub-tree is empty, return and exit
+	if(root == NULL) return;
 
-    while(i<n){
-        cin>>a;
-        q[j]=newNode(a);
-        q[(j-1)/2]->left=q[j];
-        j=j+1;i=i+1;
+	printf("%c ",root->data); // Print data
+	Preorder(root->left);     // Visit left subtree
+	Preorder(root->right);    // Visit right subtree
+}
 
-        if(i==n){
-            break;
-        }
-        cin>>a;
-        q[j]=newNode(a);
-        q[(j-1)/2]->right=q[j];
-        j=j+1;i=i+1;
-    }
-    return(q[0]);
+//Function to visit nodes in Inorder
+void Inorder(Node *root) {
+	if(root == NULL) return;
+
+	Inorder(root->left);       //Visit left subtree
+	printf("%c ",root->data);  //Print data
+	Inorder(root->right);      // Visit right subtree
 }
-void inorder(node* head){
-    node* temp=head;
-    if(temp==NULL){
-        return;
-    }
-    inorder(temp->left);
-    cout<<temp->value<<" ";
-    inorder(temp->right);
+
+//Function to visit nodes in Postorder
+void Postorder(Node *root) {
+	if(root == NULL) return;
+
+	Postorder(root->left);    // Visit left subtree
+	Postorder(root->right);   // Visit right subtree
+	printf("%c ",root->data); // Print data
 }
-void preorder(node* head){
-    node* temp=head;
-    if(temp==NULL){
-        return;
-    }
-    cout<<temp->value<<" ";
-    preorder(temp->left);
-    preorder(temp->right);
+
+// Function to Insert Node in a Binary Search Tree
+Node* Insert(Node *root,char data) {
+	if(root == NULL) {
+		root = new Node();
+		root->data = data;
+		root->left = root->right = NULL;
+	}
+	else if(data <= root->data)
+		root->left = Insert(root->left,data);
+	else 
+		root->right = Insert(root->right,data);
+	return root;
 }
-void postorder(node* head){
-    node* temp=head;
-    if(temp==NULL){
-        return;
-    }
-    postorder(temp->left);
-    postorder(temp->right);
-    cout<<temp->value<<" ";
-}
-void levelorder(node* head){
-    node* q[100];
-    int i=1,j=0;
-    q[0]=head;
-    while(q[j]!=NULL){
-        q[i]=q[j]->left;i++;
-        q[i]=q[j]->right;i++;
-        cout<<q[j]->value<<" ";j++;
-    }
-}
-int main(){
-    node* head=createTree();
-    inorder(head);cout<<"\n";
-    preorder(head);cout<<"\n";
-    postorder(head);cout<<"\n";
-    levelorder(head);cout<<"\n";
-    //do level order traversal with tree height and 
-    //reverse level order traversal with tree height
-    return 0;
+ 
+int main() {
+	/*Code To Test the logic
+	  Creating an example tree
+	                    M
+			   / \
+			  B   Q
+			 / \   \
+			A   C   Z
+    */
+	Node* root = NULL;
+	root = Insert(root,'M'); root = Insert(root,'B');
+	root = Insert(root,'Q'); root = Insert(root,'Z'); 
+	root = Insert(root,'A'); root = Insert(root,'C');
+	//Print Nodes in Preorder. 
+	cout<<"Preorder: ";
+	Preorder(root);
+	cout<<"\n";
+	//Print Nodes in Inorder
+	cout<<"Inorder: ";
+	Inorder(root);
+	cout<<"\n";
+	//Print Nodes in Postorder
+	cout<<"Postorder: ";
+	Postorder(root);
+	cout<<"\n";
 }
