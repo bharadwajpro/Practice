444003b2d5e370aa6de37c497fec7f416e185350
Krishna Bharadwaj <bharadwaj6598@gmail.com>
2018-11-01 10:32:14 +0530

    Create least_common_ancestor.cpp

---
+++ least_common_ancestor.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+	    if (root == p || root == q || root == NULL) return root;
+	    TreeNode *left = lowestCommonAncestor(root->left, p, q), *right = lowestCommonAncestor(root->right, p, q);
+	    return left && right ? root : left ? left : right;
+    }
+};
